347a8bc308f41cf7f62f3e29e9c4b8db11e4ee5b
Theotime <theotimechognika@gmail.com>
2022-04-17 21:50:09 +0200

    welcome page completed

--- lib/accueil.dart
+++ lib/accueil.dart
@@ -1,47 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:my_day_tasks/task_list.dart';
 
-class Accueil extends StatelessWidget{
+
+class Welcome extends StatelessWidget{
+  const Welcome({Key? key}) : super(key: key);
+
+  // static const Function() func = (){
+  //   Navigator.pushNamed(context, Login);
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: ListView(
-        children: [Column(
-          children: [
-            Image.asset('assets/images/welcome.png', fit: BoxFit.cover,),
-            const SizedBox(height: 15.0,),
-            const Text('Hello!',
-            textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 15.0,),
-            const Text('Welcome to your taskList App',
-            textAlign: TextAlign.center,),
-            const SizedBox(height: 15.0,),
-            Container(
-              width: 80,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(200),
-                color: Colors.deepPurple,
-            ),
-            child: const MaterialButton(
-              onPressed: null,
-              child: Text('Login'),
-            )
-            ),
-            const SizedBox(height: 15.0,),
-            Container(
-              width: 80,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(200),
-                color: Colors.deepPurple,
+        children: [Padding(
+          padding: const EdgeInsets.symmetric(vertical: 40.0, horizontal: 40.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/welcome.png', fit: BoxFit.cover,),
+              const SizedBox(height: 15.0,),
+              const Text('Hello!',
+              textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 15.0,),
+              const Text('Welcome to your taskList App',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 25.0),),
+              const SizedBox(height: 15.0,),
+              Container(
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(200),
+                  color: Colors.deepPurple,
               ),
               child: const MaterialButton(
                 onPressed: null,
-                child: Text('Sign Up'),
-              ),)
-          ],
+                child: Text('Login',
+                style: TextStyle(color: Colors.white, fontSize: 15.0)),
+              )
+              ),
+              const SizedBox(height: 15.0,),
+              Container(
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(200),
+                  color: Colors.deepPurple,
+                ),
+                child: const MaterialButton(
+                  onPressed: null,
+                  child: Text('Sign Up',
+                      style: TextStyle(color: Colors.white, fontSize: 15.0)),
+                ),)
+            ],
+          ),
         ),]
       )
       );
